777a9027f0e99937425ab8d8599b47bc1e97e9a0
chrisnm12 <119520786+chrisnm12@users.noreply.github.com>
2023-10-09 11:27:25 -0400

    Add files via upload
    Link for google doc for comms: https://docs.google.com/document/d/1djwFwIjCBFD0IqeXSQQ07lpk3qT4NJQ3giuyrIc1sjg/edit?usp=sharing

--- RoadTrip.java
+++ RoadTrip.java
@@ -1,21 +1,39 @@
-interface IRoadTrip {}
-
-class Drivers implements IRoadTrip {
+import tester.*;
+public class RoadTrip {
   String driver1;
   String driver2;
   ILoDirections directions;
-  Drivers(String driver1, String driver2, ILoDirections directions) {
+  RoadTrip(String driver1, String driver2, ILoDirections directions) {
     this.driver1 = driver1;
     this.driver2 = driver2;
     this.directions = directions;
   }
+
 }
 
-class Direction implements IRoadTrip {
+class Direction {
   String description;
   int miles;
   Direction(String description, int miles) {
     this.description = description;
     this.miles = miles;
   }
+}
+
+class RoadTripChunk {
+  String driver;
+  ILoDirections direction;
+  RoadTripChunk(String driver, ILoDirections direction){
+    driver = this.driver;
+    direction = this.direction;
+  }
+}
+
+class ExamplesRoadTrip {
+  RoadTrip Example = new RoadTrip ("Hazel", "Henry", new ConsLoDirections (new Direction ("Make a right at the fork", 2),
+    new ConsLoDirections (new Direction ("Make a left at the next fork", 3),
+    new ConsLoDirections (new Direction ("Take the overpass", 45),
+      new ConsLoDirections (new Direction ("Destination on left", 12),
+        new MtLoDirections()))))
+    );
 }
