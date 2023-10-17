3ab37c74a83c0725841d7981593e2d13eb56918d
chrisnm12 <119520786+chrisnm12@users.noreply.github.com>
2023-10-16 21:37:32 -0400

    Add files via upload
    COMBINE DONE

--- ITree.java
+++ ITree.java
@@ -9,7 +9,10 @@ interface ITree {
   WorldImage drawRight();
   WorldImage drawLeft();
   boolean isDrooping();
-  // ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree);
+  WorldImage combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree);
+  WorldImage drawCombineLeft(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree);
+  WorldImage drawCombineRight(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree);
+
 }
 
 class Leaf implements ITree {
@@ -33,9 +36,15 @@ class Leaf implements ITree {
   public boolean isDrooping() {
     return false;
   }
-  // public ITree combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
-    //return
-  // }
+  public WorldImage combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return null;
+   }
+  public WorldImage drawCombineLeft(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return null;
+  }
+  public WorldImage drawCombineRight(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return null;
+  }
 }
 
 class Stem implements ITree {
@@ -73,6 +82,15 @@ class Stem implements ITree {
   public boolean isDrooping() {
     return !((this.theta > 0) && (this.theta < 180));
   }
+  public WorldImage combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return null;
+  }
+  public WorldImage drawCombineLeft(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return null;
+  }
+  public WorldImage drawCombineRight(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return null;
+  }
 }
 
 class Branch implements ITree {
@@ -102,11 +120,9 @@ class Branch implements ITree {
         (this.drawLeft(),
           this.drawRight()));
   }
-  // ((Math.cos(leftTheta) * leftLength) + (Math.cos(rightTheta) * rightLength))
   public WorldImage drawRight() {
     return
       (new VisiblePinholeImage
-        // WRITE OVERLAY FIRST AND THEN USE ROTATE???
        (new RotateImage
           (new OverlayImage
               (new LineImage
@@ -115,7 +131,7 @@ class Branch implements ITree {
             .movePinhole(0, this.rightLength),
             (90 - this.rightTheta))
             ));
-        //.movePinhole((Math.cos (this.rightTheta) * this.rightLength), (Math.sin(this.rightTheta) * this.rightLength));
+
   }
   public WorldImage drawLeft() {
     return
@@ -132,11 +148,45 @@ class Branch implements ITree {
   public boolean isDrooping() {
     return !(((this.leftTheta > 90) && (this.leftTheta < 180)) && ((this.rightTheta > 0) && (this.rightTheta < 90)));
   }
+
+  public WorldImage combine(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return
+      (new OverlayImage
+       (this.drawCombineLeft(leftLength, rightLength, leftTheta, rightTheta, otherTree),
+         otherTree.drawCombineRight(leftLength, rightLength, leftTheta, rightTheta, otherTree)));
+  }
+  public WorldImage drawCombineLeft(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return
+      new VisiblePinholeImage
+        (new RotateImage
+          (new OverlayImage
+            (new LineImage
+              (new Posn(0, leftLength), Color.RED).movePinhole(0, -((double) leftLength / 2))
+              , this.draw()).movePinhole(0, leftLength),
+            (90 - leftTheta)
+          ));
+  }
+  public WorldImage drawCombineRight(int leftLength, int rightLength, double leftTheta, double rightTheta, ITree otherTree) {
+    return
+      (new VisiblePinholeImage
+        (new RotateImage
+          (new OverlayImage
+            (new LineImage
+              (new Posn(0, rightLength), Color.BLUE).movePinhole(0, -((double) rightLength / 2)),
+              otherTree.draw())
+            .movePinhole(0, rightLength),
+            (90 - rightTheta))
+        ));
+  }
 }
 
 class ExampleTrees {
   Leaf LeafExample1 = new Leaf(20, Color.BLUE);
+  Leaf LeafExample2 = new Leaf(30, Color.RED);
+  Leaf LeafExample3 = new Leaf(25, Color.YELLOW);
   WorldImage Leaf1 = LeafExample1.draw();
+  WorldImage Leaf2 = LeafExample2.draw();
+  WorldImage Leaf3 = LeafExample3.draw();
   Stem StemExample1 = new Stem (50, 30.0, LeafExample1);
   Stem StemExample2 = new Stem(50, 150.0, LeafExample1);
   WorldImage Stem1 = StemExample1.draw();
@@ -145,11 +195,17 @@ class ExampleTrees {
   Branch BranchExample2 = new Branch(70, 50, 120.0, 330.0, LeafExample1, LeafExample1);
   WorldImage Branch1 = BranchExample1.draw();
   WorldImage Branch2 = BranchExample2.draw();
-  // WorldImage Tree1 = new BesideImage(this.LeafExample.draw(), this.StemExample.draw());
+  ITree Tree1 = new Branch (50, 50, 145, 45, LeafExample2, LeafExample1);
+
+  ITree Tree2 = new Branch (50, 50, 155, 55, LeafExample1, LeafExample2);
+  ITree Tree3 = new Branch (100, 100, 160, 330, LeafExample1, LeafExample3);
+  WorldImage Combine1 = Tree1.combine(50, 50, 135, 45, Tree2);
+  WorldImage Combine2 = Tree1.combine(60, 60, 150, 330, Tree3);
+
   boolean testDrawTree(Tester t) {
     WorldCanvas c = new WorldCanvas(500, 500);
     WorldScene s = new WorldScene(500, 500);
-    return c.drawScene(s.placeImageXY(Branch2, 250, 250))
+    return c.drawScene(s.placeImageXY(Combine2, 250, 250))
       && c.show();
   }
   /*
